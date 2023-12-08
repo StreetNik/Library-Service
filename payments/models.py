41003620bad17e7ef327ac801fb7633fa8b60a0e
Stanislav Nikulytsia <nikulicastas2004@gmail.com>
2023-12-08 03:01:14 -0600

    Extend max length of session url field to 500

--- payments/models.py
+++ payments/models.py
@@ -11,6 +11,6 @@ class Payment(models.Model):
     status = models.CharField(choices=PAYMENT_STATUS_CHOICES, default="PENDING", max_length=144)
     type = models.CharField(choices=PAYMENT_TYPE_CHOICES, default="PAYMENT", max_length=144)
     borrowing = models.ForeignKey(Borrowing, on_delete=models.CASCADE, related_name="payments")
-    session_url = models.URLField()
+    session_url = models.URLField(max_length=500)
     session_id = models.CharField(max_length=240)
     money_to_pay = models.DecimalField(max_digits=8, decimal_places=2)
